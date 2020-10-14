5d64ecc9318c91b0d12eb4b13474a152c30f4611
Vinicius Matias <45612047+matiasvinicius@users.noreply.github.com>
2020-10-14 15:24:11 -0300

    data_transfer_tcp_udp.cc

---
+++ data_transfer_tcp_udp.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/ipv4-static-routing-helper.h"
+#include "ns3/ipv4-list-routing-helper.h"
+
+using namespace ns3;
