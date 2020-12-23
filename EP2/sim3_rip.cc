328977e39a53f60b4d28605cd0fbe890bfa27783
Vinicius Matias <45612047+matiasvinicius@users.noreply.github.com>
2020-12-23 06:01:20 -0300

    Update and rename csma_rip.cc to sim3_rip.cc

--- EP2/sim3_rip.cc
+++ EP2/sim3_rip.cc
@@ -7,10 +7,12 @@
 #include "ns3/ipv4-global-routing-helper.h"
 #include "ns3/csma-module.h"
 
+
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("simulacao");//permite a adoção de logs durante o código
 
+//Função baseada nos exemplos do ns3 para derrubar o link entre dois pontos
 void TearDownLink (Ptr<Node> nodeA, Ptr<Node> nodeB, uint32_t interfaceA, uint32_t interfaceB)
 {
   nodeA->GetObject<Ipv4> ()->SetDown (interfaceA);
@@ -19,10 +21,13 @@ void TearDownLink (Ptr<Node> nodeA, Ptr<Node> nodeB, uint32_t interfaceA, uint32
 
 int main(int argc, char *argv[]){
   std::string SplitHorizon ("PoisonReverse");
-  //CommandLine cmd;
-  //LogComponentEnable ("Rip", LOG_LEVEL_ALL);
-  //cmd.Parse (argc, argv);
   Config::SetDefault ("ns3::Rip::SplitHorizon", EnumValue (RipNg::POISON_REVERSE));
+  /*HABILITA A VISÃO DOS LOGS NO TERMINAL (CABEÇALHOS)
+  CommandLine cmd;
+  LogComponentEnable ("Rip", LOG_LEVEL_ALL);
+  md.AddValue ("verbose", "turn on log components", true);
+  cmd.Parse (argc, argv);
+  */
 
   /*
   Criação dos 35 nós, armazenados em uma estrutura de dados "NodeContainer"
@@ -37,7 +42,6 @@ int main(int argc, char *argv[]){
   NodeContainer routers;
   hosts.Create(totalHosts); 
   routers.Create(totalRouters);
-
   /*
   Criação dos 44 enlaces para os 35 nós
   Aqui separamos os disposivos criados em conjunto e definimos com que máquina cada um se comunicará
@@ -51,49 +55,47 @@ int main(int argc, char *argv[]){
 
   //------------Configuração dos enlaces--------------------
   //Enlaces entre hosts
-  //Rede 1 -> AS1
+  //Rede 1
   NodeContainer h0h1 = NodeContainer(hosts.Get(0), hosts.Get(1));
   NodeContainer h0h2 = NodeContainer(hosts.Get(0), hosts.Get(2));
   NodeContainer h0h3 = NodeContainer(hosts.Get(0), hosts.Get(3));
-  NodeContainer h1h2 = NodeContainer(hosts.Get(1), hosts.Get(2));
   NodeContainer h1h4 = NodeContainer(hosts.Get(1), hosts.Get(4));
   NodeContainer h2h3 = NodeContainer(hosts.Get(2), hosts.Get(3));
   NodeContainer h2h4 = NodeContainer(hosts.Get(2), hosts.Get(4));
   NodeContainer h3h4 = NodeContainer(hosts.Get(3), hosts.Get(4));
 
-  //Rede 2 -> AS1
+  //Rede 2
   NodeContainer h5h6 = NodeContainer(hosts.Get(5), hosts.Get(6));
   NodeContainer h6h7 = NodeContainer(hosts.Get(6), hosts.Get(7));
 
-  //Rede 3 -> AS1
+  //Rede 3
   NodeContainer h8h9 = NodeContainer(hosts.Get(8), hosts.Get(9));
 
-  //Rede 4 -> AS2
+  //Rede 4
   NodeContainer h10h11 = NodeContainer(hosts.Get(10), hosts.Get(11));
   NodeContainer h10h12 = NodeContainer(hosts.Get(10), hosts.Get(12));
   NodeContainer h10h13 = NodeContainer(hosts.Get(10), hosts.Get(13));
   NodeContainer h12h13 = NodeContainer(hosts.Get(12), hosts.Get(13));
 
-  //Rede 5 -> AS2
+  //Rede 5
   NodeContainer h14h15 = NodeContainer(hosts.Get(14), hosts.Get(15));
   
-  //Rede 6 -> AS3
+  //Rede 6
   NodeContainer h16h17 = NodeContainer(hosts.Get(16), hosts.Get(17));
   
-  //Rede 7 -> AS3
+  //Rede 7
   NodeContainer h18h19 = NodeContainer(hosts.Get(18), hosts.Get(19));
   NodeContainer h18h20 = NodeContainer(hosts.Get(18), hosts.Get(20));
   NodeContainer h19h20 = NodeContainer(hosts.Get(19), hosts.Get(20));
 
-  //Rede 8 -> AS3
+  //Rede 8
   NodeContainer h21h22 = NodeContainer(hosts.Get(21), hosts.Get(22));
   NodeContainer h21h23 = NodeContainer(hosts.Get(21), hosts.Get(23));
   NodeContainer h22h23 = NodeContainer(hosts.Get(22), hosts.Get(23));
   NodeContainer h21h24 = NodeContainer(hosts.Get(21), hosts.Get(24));
   NodeContainer h24h25 = NodeContainer(hosts.Get(24), hosts.Get(25));
 
-  //Elaces entre os roteadores (intra-AS)
-  //AS1
+  //Elaces entre os roteadores
   NodeContainer r0r1 = NodeContainer(routers.Get(0), routers.Get(1));
   NodeContainer r0r2 = NodeContainer(routers.Get(0), routers.Get(2));
   NodeContainer r0r3 = NodeContainer(routers.Get(0), routers.Get(3));
@@ -101,38 +103,31 @@ int main(int argc, char *argv[]){
   NodeContainer r1r3 = NodeContainer(routers.Get(1), routers.Get(3));
   NodeContainer r2r3 = NodeContainer(routers.Get(2), routers.Get(3));
 
-  //AS2
   NodeContainer r4r5 = NodeContainer(routers.Get(4), routers.Get(5));
 
-  //AS3
   NodeContainer r6r7 = NodeContainer(routers.Get(6), routers.Get(7));
   NodeContainer r6r8 = NodeContainer(routers.Get(6), routers.Get(8));
   NodeContainer r7r8 = NodeContainer(routers.Get(7), routers.Get(8));
 
-  //Enlaces entre os roteadores(inter-AS)
-  //AS1 - AS2
   NodeContainer r3r4 = NodeContainer(routers.Get(3), routers.Get(4));
 
-  //AS2 - AS3
   NodeContainer r5r6 = NodeContainer(routers.Get(5), routers.Get(6));
   
   //Enlaces entre Roteador e Host
-  //AS1
   NodeContainer r0h0 = NodeContainer(routers.Get(0), hosts.Get(0));
   NodeContainer r1h5 = NodeContainer(routers.Get(1), hosts.Get(5));
   NodeContainer r2h8 = NodeContainer(routers.Get(2), hosts.Get(8));
 
-  //AS2
   NodeContainer r4h10 = NodeContainer(routers.Get(4), hosts.Get(10));
   NodeContainer r5h14 = NodeContainer(routers.Get(5), hosts.Get(14));
 
-  //AS3
   NodeContainer r6h16 = NodeContainer(routers.Get(6), hosts.Get(16));
   NodeContainer r7h18 = NodeContainer(routers.Get(7), hosts.Get(18));
   NodeContainer r8h21 = NodeContainer(routers.Get(8), hosts.Get(21));
   
   //---------------Configuração dos canais------------- 
-  //Vão existir 5 configurações de canais para os Enlaces
+  //Existem 2 configurações de canais para os Enlaces
+  //Os resultados das análsies do relatório utilzaram apenas o channelLevel2
   PointToPointHelper channelLevel1;
   channelLevel1.SetDeviceAttribute("DataRate", StringValue("128kbps"));
   channelLevel1.SetChannelAttribute("Delay", StringValue("10ms"));
@@ -141,49 +136,36 @@ int main(int argc, char *argv[]){
   channelLevel2.SetChannelAttribute("DataRate", StringValue("5Mbps"));
   channelLevel2.SetChannelAttribute("Delay", TimeValue(MilliSeconds(2)));
 
-  PointToPointHelper channelLevel3;
-  channelLevel3.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
-  channelLevel3.SetChannelAttribute("Delay", StringValue("1ms"));
-
-  PointToPointHelper channelLevel4;
-  channelLevel4.SetDeviceAttribute("DataRate", StringValue("50Mbps"));
-  channelLevel4.SetChannelAttribute("Delay", StringValue("1ms"));
-
-  PointToPointHelper channelLevel5;
-  channelLevel5.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
-  channelLevel5.SetChannelAttribute("Delay", StringValue("0ms"));
-
   //--------Atribuição dos canais aos enlaces----------
-  //Rede 1 -> AS1
+  //Rede 1 
   NetDeviceContainer h0h1_link = channelLevel2.Install(h0h1);
   NetDeviceContainer h0h2_link = channelLevel2.Install(h0h2);
   NetDeviceContainer h0h3_link = channelLevel2.Install(h0h3);
-  //NetDeviceContainer h1h2_link = channelLevel2.Install(h1h2);
   NetDeviceContainer h1h4_link = channelLevel2.Install(h1h4);
   NetDeviceContainer h2h3_link = channelLevel2.Install(h2h3);
   NetDeviceContainer h2h4_link = channelLevel2.Install(h2h4);
   NetDeviceContainer h3h4_link = channelLevel2.Install(h3h4);
   
-  //Rede 2 -> AS1
+  //Rede 2
   NetDeviceContainer h5h6_link = channelLevel2.Install(h5h6);
   NetDeviceContainer h6h7_link = channelLevel2.Install(h6h7);
 
-  //Rede 3 -> AS1
+  //Rede 3
   NetDeviceContainer h8h9_link = channelLevel2.Install(h8h9);
 
-  //Rede 4 -> AS2
+  //Rede 4
   NetDeviceContainer h10h11_link = channelLevel2.Install(h10h11);
   NetDeviceContainer h10h12_link = channelLevel2.Install(h10h12);
   NetDeviceContainer h10h13_link = channelLevel2.Install(h10h13);
   NetDeviceContainer h12h13_link = channelLevel2.Install(h12h13);
 
-  //Rede 5 -> AS2
+  //Rede 5
   NetDeviceContainer h14h15_link = channelLevel2.Install(h14h15);
 
-  //Rede 6 -> AS3
+  //Rede 6
   NetDeviceContainer h16h17_link = channelLevel2.Install(h16h17);
 
-  //Rede 7 -> AS3
+  //Rede 7
   NetDeviceContainer h18h19_link = channelLevel2.Install(h18h19);
   NetDeviceContainer h18h20_link = channelLevel2.Install(h18h20);
   NetDeviceContainer h19h20_link = channelLevel2.Install(h19h20);
@@ -195,8 +177,7 @@ int main(int argc, char *argv[]){
   NetDeviceContainer h21h24_link = channelLevel2.Install(h21h24);
   NetDeviceContainer h24h25_link = channelLevel2.Install(h24h25);
   
-  //Elaces entre os roteadores (intra-AS)
-  //AS1  
+  //Elaces entre os roteadores
   NetDeviceContainer r0r1_link = channelLevel2.Install(r0r1);
   NetDeviceContainer r0r2_link = channelLevel2.Install(r0r2);
   NetDeviceContainer r0r3_link = channelLevel2.Install(r0r3);
@@ -204,53 +185,41 @@ int main(int argc, char *argv[]){
   NetDeviceContainer r1r3_link = channelLevel2.Install(r1r3);
   NetDeviceContainer r2r3_link = channelLevel2.Install(r2r3);
 
-  //AS2
   NetDeviceContainer r4r5_link = channelLevel2.Install(r4r5);
   
-  //AS3
   NetDeviceContainer r6r7_link = channelLevel2.Install(r6r7);
   NetDeviceContainer r6r8_link = channelLevel2.Install(r6r8);
   NetDeviceContainer r7r8_link = channelLevel2.Install(r7r8);
 
-  //Enlaces entre os roteadores(inter-AS)
-  //AS1 - AS2
   NetDeviceContainer r3r4_link = channelLevel2.Install(r3r4);
-  
-  //AS2 - AS3
+
   NetDeviceContainer r5r6_link = channelLevel2.Install(r5r6);
   
   //Enlaces entre Roteador e Host
-  //AS1
   NetDeviceContainer r0h0_link = channelLevel2.Install(r0h0);
   NetDeviceContainer r1h5_link = channelLevel2.Install(r1h5);
   NetDeviceContainer r2h8_link = channelLevel2.Install(r2h8);
  
-  //AS2
   NetDeviceContainer r4h10_link = channelLevel2.Install(r4h10);
   NetDeviceContainer r5h14_link = channelLevel2.Install(r5h14);
 
-  //AS3
   NetDeviceContainer r6h16_link = channelLevel2.Install(r6h16);
   NetDeviceContainer r7h18_link = channelLevel2.Install(r7h18);
   NetDeviceContainer r8h21_link = channelLevel2.Install(r8h21); 
   
+  //---------Instalação da Pilha de Internet----------
+  //Adiciona por padrão o protocolo IPv4, UDP e TCP nos hosts
+  InternetStackHelper internetHosts;
+  internetHosts.SetIpv6StackInstall (false);
+  for(int i = 0; i < totalHosts; i++){
+    internetHosts.Install(hosts.Get(i));
+  }
+
+  //Instalação da pilha com adição do protocolo RIP nos roteadores
   RipHelper ripRouting;
-  /*for (int i=0; i<totalHosts; i++){
-    ripRouting.ExcludeInterface (hosts.Get(i), 1);
-  }*/
-
-  /*for(int i=0; i < totalRouters; i++){
-    ripRouting.SetInterfaceMetric (routers.Get(i), 1, 10);
-  }*/
-
-  //ripRouting.ExcludeInterface (routers.Get(0), 2);
-  //ripRouting.SetInterfaceMetric (routers.Get(0), 2, 10);
-  //ripRouting.SetInterfaceMetric (routers.Get(0), 3, 2);
-
-
   Ipv4ListRoutingHelper listRH;
-  listRH.Add (ripRouting, 0);
 
+  listRH.Add (ripRouting, 0);
   InternetStackHelper internetRouters;
   internetRouters.SetIpv6StackInstall (false);
   internetRouters.SetRoutingHelper (listRH);
@@ -258,21 +227,10 @@ int main(int argc, char *argv[]){
     internetRouters.Install(routers.Get(i));
   }
 
-
-  //---------Pilha de Internet----------
-  //Instala pilha de Internet (permite o uso de protocolos TCP, UDP e IP)
-  InternetStackHelper internetHosts;
-  internetHosts.SetIpv6StackInstall (false);
-  for(int i = 0; i < totalHosts; i++){
-    internetHosts.Install(hosts.Get(i));
-  }
-
-
   //-------------Atribuição dos endereços IP----------------
   Ipv4AddressHelper address;
 
-  //Elaces entre os roteadores (intra-AS)
-  //AS1
+  //Elaces entre os roteadores
   address.SetBase("192.168.2.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r0r1_link);
 
@@ -291,11 +249,9 @@ int main(int argc, char *argv[]){
   address.SetBase("192.168.1.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r1r3_link);  
 
-  //AS2
   address.SetBase("192.169.0.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r4r5_link);  
 
-  //AS3
   address.SetBase("192.170.0.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r6r7_link);  
 
@@ -305,17 +261,13 @@ int main(int argc, char *argv[]){
   address.SetBase("192.170.2.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r6r8_link);   
 
-  //Enlaces entre os roteadores(inter-AS)
-  //AS1 - AS2
   address.SetBase("192.1.0.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r3r4_link);   
 
-  //AS2 - AS3
   address.SetBase("192.2.0.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r5r6_link);   
 
   //Enlaces entre Roteador e Host
-  //AS1
   address.SetBase("192.168.7.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r0h0_link);   
 
@@ -325,14 +277,12 @@ int main(int argc, char *argv[]){
   address.SetBase("192.168.6.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r2h8_link);
 
-  //AS2
   address.SetBase("192.169.1.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r4h10_link);
 
   address.SetBase("192.169.2.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r5h14_link);
 
-  //AS3  
   address.SetBase("192.170.5.0", "255.255.255.0", "0.0.0.0");
   address.Assign (r6h16_link);
   
@@ -343,7 +293,7 @@ int main(int argc, char *argv[]){
   address.Assign (r7h18_link);
 
   //IPs dos enlaces entre hosts
-  //Rede 1 -> AS1
+  //Rede 1
   address.SetBase("192.168.7.0", "255.255.255.0","0.0.0.2");
   address.Assign (h0h3_link);
 
@@ -365,21 +315,18 @@ int main(int argc, char *argv[]){
   address.SetBase("192.168.7.0", "255.255.255.0", "0.0.0.14");
   address.Assign (h2h4_link);
 
-  //address.SetBase("192.168.7.0", "255.255.255.0", "0.0.0.16");
-  //address.Assign (h1h2_link);
-
-  //Rede 2 -> AS1
+  //Rede 2
   address.SetBase("192.168.8.0", "255.255.255.0", "0.0.0.2");
   address.Assign (h5h6_link); 
 
   address.SetBase("192.168.8.0", "255.255.255.0", "0.0.0.4");
   address.Assign (h6h7_link);
 
-  //Rede 3 -> AS1
+  //Rede 3
   address.SetBase("192.168.6.0", "255.255.255.0", "0.0.0.2");
   address.Assign (h8h9_link); 
 
-  //Rede 4 -> AS2
+  //Rede 4
   address.SetBase("192.169.1.0", "255.255.255.0", "0.0.0.2");
   address.Assign (h10h11_link);  
 
@@ -392,15 +339,15 @@ int main(int argc, char *argv[]){
   address.SetBase("192.169.1.0", "255.255.255.0", "0.0.0.8");
   address.Assign (h12h13_link); 
 
-  //Rede 5 -> AS2
+  //Rede 5
   address.SetBase("192.169.2.0", "255.255.255.0", "0.0.0.2");
   address.Assign (h14h15_link); 
 
-  //Rede 6 -> AS3
+  //Rede 6
   address.SetBase("192.170.5.0", "255.255.255.0","0.0.0.2");
   address.Assign (h16h17_link);
 
-  //Rede 7 -> AS3
+  //Rede 7
   address.SetBase("192.170.4.0", "255.255.255.0", "0.0.0.2");
   address.Assign (h18h19_link); 
 
@@ -410,7 +357,7 @@ int main(int argc, char *argv[]){
   address.SetBase("192.170.4.0", "255.255.255.0", "0.0.0.6");
   address.Assign (h18h20_link); 
 
-  //Rede 8 -> AS3
+  //Rede 8
   address.SetBase("192.170.3.0", "255.255.255.0", "0.0.0.2");
   address.Assign (h21h23_link); 
 
@@ -425,32 +372,30 @@ int main(int argc, char *argv[]){
 
   address.SetBase("192.170.3.0", "255.255.255.0", "0.0.0.10");
   Ipv4InterfaceContainer destinatario = address.Assign (h24h25_link);
-  
-  //--------Print das tabelas de roteamento--------
 
+  //----CAPTURA DA TABELA DE ROTEAMENTO--------- 
   Ptr<Ipv4StaticRouting> staticRouting;
   staticRouting = Ipv4RoutingHelper::GetRouting <Ipv4StaticRouting> (routers.Get(0)->GetObject<Ipv4> ()->GetRoutingProtocol ());
-  //staticRouting->SetDefaultRoute ("192.168.7.0", 1 );
   staticRouting = Ipv4RoutingHelper::GetRouting <Ipv4StaticRouting> (routers.Get(7)->GetObject<Ipv4> ()->GetRoutingProtocol ());
-  //staticRouting->SetDefaultRoute ("192.1.0.1", 1 );
 
   RipHelper routingHelper;
   Ptr<OutputStreamWrapper> routingStream = Create<OutputStreamWrapper> (&std::cout);
 
+  //Log no terminal das tabelas de roteamento nos segundos 0, 5, 20 e 100 para o roteador 0
   routingHelper.PrintRoutingTableAt (Seconds (0), routers.Get(0), routingStream);
   routingHelper.PrintRoutingTableAt (Seconds (5), routers.Get(0), routingStream);
   routingHelper.PrintRoutingTableAt (Seconds (20), routers.Get(0), routingStream);
   routingHelper.PrintRoutingTableAt (Seconds (100), routers.Get(0), routingStream);
 
-
+  //Rota realizada no arquivo .tr
+  AsciiTraceHelper ascii;
+  channelLevel2.EnableAsciiAll (ascii.CreateFileStream ("rota_rip.tr"));
 
   //Tabela de Roteamento
   Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-  //--------Animação do NetAnim--------
-
   //Gera xml para usar no NetAnim
-  AnimationInterface anim ("sim4_rip.xml");
+  AnimationInterface anim ("sim2_rip.xml");
 
   //define posições do(s) node(s) P2P no NetAnim
   anim.SetConstantPosition (h0h3.Get(0), 25.0, 50.0);
@@ -489,8 +434,8 @@ int main(int argc, char *argv[]){
   anim.SetConstantPosition (r6r7.Get(1), 15.0, 20.0);
   anim.SetConstantPosition (r6r8.Get(1), 20.0, 15.0);
 
-  //---------Simulação-----------
 
+  //Finaliza simulação após 200 segundos
   Simulator::Stop (Seconds (200.0));
   Simulator::Run();
   Simulator::Destroy();
