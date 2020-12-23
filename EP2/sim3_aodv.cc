691c6e2d739f946983e2e104854fc03809e877b6
Vinicius Matias <45612047+matiasvinicius@users.noreply.github.com>
2020-12-23 06:11:38 -0300

    Update sim3_aodv.cc

--- EP2/sim3_aodv.cc
+++ EP2/sim3_aodv.cc
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]){
 
   //------------Configuração dos enlaces--------------------
   //Enlaces entre hosts
-  //Rede 1 -> AS1
+  //Rede 1
   NodeContainer h0h1 = NodeContainer(hosts.Get(0), hosts.Get(1));
   NodeContainer h0h2 = NodeContainer(hosts.Get(0), hosts.Get(2));
   NodeContainer h0h3 = NodeContainer(hosts.Get(0), hosts.Get(3));
@@ -51,39 +51,38 @@ int main(int argc, char *argv[]){
   NodeContainer h2h4 = NodeContainer(hosts.Get(2), hosts.Get(4));
   NodeContainer h3h4 = NodeContainer(hosts.Get(3), hosts.Get(4));
 
-  //Rede 2 -> AS1
+  //Rede 2
   NodeContainer h5h6 = NodeContainer(hosts.Get(5), hosts.Get(6));
   NodeContainer h6h7 = NodeContainer(hosts.Get(6), hosts.Get(7));
 
-  //Rede 3 -> AS1
+  //Rede 3
   NodeContainer h8h9 = NodeContainer(hosts.Get(8), hosts.Get(9));
 
-  //Rede 4 -> AS2
+  //Rede 4
   NodeContainer h10h11 = NodeContainer(hosts.Get(10), hosts.Get(11));
   NodeContainer h10h12 = NodeContainer(hosts.Get(10), hosts.Get(12));
   NodeContainer h10h13 = NodeContainer(hosts.Get(10), hosts.Get(13));
   NodeContainer h12h13 = NodeContainer(hosts.Get(12), hosts.Get(13));
 
-  //Rede 5 -> AS2
+  //Rede 5
   NodeContainer h14h15 = NodeContainer(hosts.Get(14), hosts.Get(15));
   
-  //Rede 6 -> AS3
+  //Rede 6
   NodeContainer h16h17 = NodeContainer(hosts.Get(16), hosts.Get(17));
   
-  //Rede 7 -> AS3
+  //Rede 7
   NodeContainer h18h19 = NodeContainer(hosts.Get(18), hosts.Get(19));
   NodeContainer h18h20 = NodeContainer(hosts.Get(18), hosts.Get(20));
   NodeContainer h19h20 = NodeContainer(hosts.Get(19), hosts.Get(20));
 
-  //Rede 8 -> AS3
+  //Rede 8
   NodeContainer h21h22 = NodeContainer(hosts.Get(21), hosts.Get(22));
   NodeContainer h21h23 = NodeContainer(hosts.Get(21), hosts.Get(23));
   NodeContainer h22h23 = NodeContainer(hosts.Get(22), hosts.Get(23));
   NodeContainer h21h24 = NodeContainer(hosts.Get(21), hosts.Get(24));
   NodeContainer h24h25 = NodeContainer(hosts.Get(24), hosts.Get(25));
 
-  //Elaces entre os roteadores (intra-AS)
-  //AS1
+  //Elaces entre os roteadores
   NodeContainer r0r1 = NodeContainer(routers.Get(0), routers.Get(1));
   NodeContainer r0r2 = NodeContainer(routers.Get(0), routers.Get(2));
   NodeContainer r0r3 = NodeContainer(routers.Get(0), routers.Get(3));
@@ -91,32 +90,24 @@ int main(int argc, char *argv[]){
   NodeContainer r1r3 = NodeContainer(routers.Get(1), routers.Get(3));
   NodeContainer r2r3 = NodeContainer(routers.Get(2), routers.Get(3));
 
-  //AS2
   NodeContainer r4r5 = NodeContainer(routers.Get(4), routers.Get(5));
 
-  //AS3
   NodeContainer r6r7 = NodeContainer(routers.Get(6), routers.Get(7));
   NodeContainer r6r8 = NodeContainer(routers.Get(6), routers.Get(8));
   NodeContainer r7r8 = NodeContainer(routers.Get(7), routers.Get(8));
 
-  //Enlaces entre os roteadores(inter-AS)
-  //AS1 - AS2
   NodeContainer r3r4 = NodeContainer(routers.Get(3), routers.Get(4));
 
-  //AS2 - AS3
   NodeContainer r5r6 = NodeContainer(routers.Get(5), routers.Get(6));
   
   //Enlaces entre Roteador e Host
-  //AS1
   NodeContainer r0h0 = NodeContainer(routers.Get(0), hosts.Get(0));
   NodeContainer r1h5 = NodeContainer(routers.Get(1), hosts.Get(5));
   NodeContainer r2h8 = NodeContainer(routers.Get(2), hosts.Get(8));
 
-  //AS2
   NodeContainer r4h10 = NodeContainer(routers.Get(4), hosts.Get(10));
   NodeContainer r5h14 = NodeContainer(routers.Get(5), hosts.Get(14));
 
-  //AS3
   NodeContainer r6h16 = NodeContainer(routers.Get(6), hosts.Get(16));
   NodeContainer r7h18 = NodeContainer(routers.Get(7), hosts.Get(18));
   NodeContainer r8h21 = NodeContainer(routers.Get(8), hosts.Get(21));
@@ -133,49 +124,47 @@ int main(int argc, char *argv[]){
 
 
   //--------Atribuição dos canais aos enlaces----------
-  //Rede 1 -> AS1
+  //Rede 1
   NetDeviceContainer h0h1_link = channelLevel2.Install(h0h1);
   NetDeviceContainer h0h2_link = channelLevel2.Install(h0h2);
   NetDeviceContainer h0h3_link = channelLevel2.Install(h0h3);
-  //NetDeviceContainer h1h2_link = channelLevel2.Install(h1h2); removido devido problemas
   NetDeviceContainer h1h4_link = channelLevel2.Install(h1h4);
   NetDeviceContainer h2h3_link = channelLevel2.Install(h2h3);
   NetDeviceContainer h2h4_link = channelLevel2.Install(h2h4);
   NetDeviceContainer h3h4_link = channelLevel2.Install(h3h4);
   
-  //Rede 2 -> AS1
+  //Rede 2
   NetDeviceContainer h5h6_link = channelLevel2.Install(h5h6);
   NetDeviceContainer h6h7_link = channelLevel2.Install(h6h7);
 
-  //Rede 3 -> AS1
+  //Rede 3
   NetDeviceContainer h8h9_link = channelLevel2.Install(h8h9);
 
-  //Rede 4 -> AS2
+  //Rede 4
   NetDeviceContainer h10h11_link = channelLevel2.Install(h10h11);
   NetDeviceContainer h10h12_link = channelLevel2.Install(h10h12);
   NetDeviceContainer h10h13_link = channelLevel2.Install(h10h13);
   NetDeviceContainer h12h13_link = channelLevel2.Install(h12h13);
 
-  //Rede 5 -> AS2
+  //Rede 5
   NetDeviceContainer h14h15_link = channelLevel2.Install(h14h15);
 
-  //Rede 6 -> AS3
+  //Rede 6
   NetDeviceContainer h16h17_link = channelLevel2.Install(h16h17);
 
-  //Rede 7 -> AS3
+  //Rede 7
   NetDeviceContainer h18h19_link = channelLevel2.Install(h18h19);
   NetDeviceContainer h18h20_link = channelLevel2.Install(h18h20);
   NetDeviceContainer h19h20_link = channelLevel2.Install(h19h20);
   
-  //Rede 8 -> AS3
+  //Rede 8
   NetDeviceContainer h21h22_link = channelLevel2.Install(h21h22);
   NetDeviceContainer h21h23_link = channelLevel2.Install(h21h23);
   NetDeviceContainer h22h23_link = channelLevel2.Install(h22h23);
   NetDeviceContainer h21h24_link = channelLevel2.Install(h21h24);
   NetDeviceContainer h24h25_link = channelLevel2.Install(h24h25);
   
-  //Elaces entre os roteadores (intra-AS)
-  //AS1  
+  //Elaces entre os roteadores
   NetDeviceContainer r0r1_link = channelLevel2.Install(r0r1);
   NetDeviceContainer r0r2_link = channelLevel2.Install(r0r2);
   NetDeviceContainer r0r3_link = channelLevel2.Install(r0r3);
@@ -183,32 +172,24 @@ int main(int argc, char *argv[]){
   NetDeviceContainer r1r3_link = channelLevel2.Install(r1r3);
   NetDeviceContainer r2r3_link = channelLevel2.Install(r2r3);
 
-  //AS2
   NetDeviceContainer r4r5_link = channelLevel2.Install(r4r5);
-  
-  //AS3
+
   NetDeviceContainer r6r7_link = channelLevel2.Install(r6r7);
   NetDeviceContainer r6r8_link = channelLevel2.Install(r6r8);
   NetDeviceContainer r7r8_link = channelLevel2.Install(r7r8);
 
-  //Enlaces entre os roteadores(inter-AS)
-  //AS1 - AS2
   NetDeviceContainer r3r4_link = channelLevel2.Install(r3r4);
   
-  //AS2 - AS3
   NetDeviceContainer r5r6_link = channelLevel2.Install(r5r6);
   
   //Enlaces entre Roteador e Host
-  //AS1
   NetDeviceContainer r0h0_link = channelLevel2.Install(r0h0);
   NetDeviceContainer r1h5_link = channelLevel2.Install(r1h5);
   NetDeviceContainer r2h8_link = channelLevel2.Install(r2h8);
  
-  //AS2
   NetDeviceContainer r4h10_link = channelLevel2.Install(r4h10);
   NetDeviceContainer r5h14_link = channelLevel2.Install(r5h14);
 
-  //AS3
   NetDeviceContainer r6h16_link = channelLevel2.Install(r6h16);
   NetDeviceContainer r7h18_link = channelLevel2.Install(r7h18);
   NetDeviceContainer r8h21_link = channelLevel2.Install(r8h21);
@@ -402,9 +383,7 @@ int main(int argc, char *argv[]){
 
   Ptr<Ipv4StaticRouting> staticRouting;
   staticRouting = Ipv4RoutingHelper::GetRouting <Ipv4StaticRouting> (hosts.Get(0)->GetObject<Ipv4> ()->GetRoutingProtocol ());
-  //staticRouting->SetDefaultRoute ("192.168.7.0", 1 );
   staticRouting = Ipv4RoutingHelper::GetRouting <Ipv4StaticRouting> (hosts.Get(25)->GetObject<Ipv4> ()->GetRoutingProtocol ());
-  //staticRouting->SetDefaultRoute ("192.1.0.1", 1 );
 
 
   AodvHelper aodvHelper;
